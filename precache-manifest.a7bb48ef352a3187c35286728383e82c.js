b09a681b7ef6d001df5b2063734ad2e4a7248e0d
Aldin38 <aldin_mikara@hotmail.com>
2020-06-07 18:24:09 +0200

    Updates

---
+++ precache-manifest.a7bb48ef352a3187c35286728383e82c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bdfae99de89c67a2194884d7907011",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "ec5c8c87f7ccaf3e1feb",
+    "url": "/Calculator/static/css/main.d4364941.chunk.css"
+  },
+  {
+    "revision": "adbbf8ce93ef39d9aa4a",
+    "url": "/Calculator/static/js/2.052a3bf8.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Calculator/static/js/2.052a3bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5c8c87f7ccaf3e1feb",
+    "url": "/Calculator/static/js/main.12dae779.chunk.js"
+  },
+  {
+    "revision": "36cda1db55e6e62de084",
+    "url": "/Calculator/static/js/runtime-main.c4967e62.js"
+  }
+]);
